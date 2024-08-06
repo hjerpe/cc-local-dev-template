03f412d16f2e139dd441fa05a75fb9f39273b0cd
Adam Hjerpe <hjerpe.adam@gmail.com>
2024-08-06 08:40:42 +0200

    add base image

--- {{ cookiecutter.project_slug }}/docker-build.sh
+++ {{ cookiecutter.project_slug }}/docker-build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-
 # stop on error
 set -e
+
 source docker-name.sh
 
-docker build -f Dockerfile -t "$image_name" .
+# Build the base image
+docker build -f Dockerfile.base -t hjerpe/python-base:latest .
+
+# Build the project-specific image
+docker build --build-arg BASE_IMAGE=hjerpe/python-base:latest -f Dockerfile -t "$image_name" .
