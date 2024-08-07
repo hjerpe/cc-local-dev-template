e3b9aac7803e5b088b9c30fc558ab0bcd0038674
Adam Hjerpe <hjerpe.adam@gmail.com>
2024-08-07 07:27:10 +0200

    add base image

--- {{ cookiecutter.project_slug }}/docker-build.sh
+++ {{ cookiecutter.project_slug }}/docker-build.sh
@@ -4,8 +4,19 @@ set -e
 
 source docker-name.sh
 
-# Build the base image
-docker build -f Dockerfile.base -t hjerpe/python-base:latest .
+# Extract the base name and tag
+base_name=$(echo $image_name | cut -d':' -f1)
+tag=$(echo $image_name | cut -d':' -f2)
 
-# Build the project-specific image
-docker build --build-arg BASE_IMAGE=hjerpe/python-base:latest -f Dockerfile -t "$image_name" .
+# Build the base image
+docker build -f Dockerfile.base -t "${base_name}-base:${tag}" .
+
+# If the image type is with_pytorch, build the PyTorch image
+if [[ $image_name == *"with_pytorch"* ]]; then
+    docker build --build-arg BASE_IMAGE="${base_name}-base:${tag}" -f Dockerfile.pytorch -t "$image_name" .
+else
+    # For the default image, we just tag the base image
+    docker tag "${base_name}-base:${tag}" "$image_name"
+fi
+
+echo "Built image: $image_name"
